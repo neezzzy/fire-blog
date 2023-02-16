329343331e540bc282053bf6ab1fdff4f3140777
Ruslan Nizamov <nizamov.ruslan@gmail.com>
2023-02-15 16:54:11 -0800

    add navigation toast routing loader

---
+++ components/Navbar.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface User {
+  photoURL: string
+}
+
+// Top navbar
+export default function Navbar() {
+  const user: User | null = null;
+  const username: string | null = null;
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
+
+        {/* user is signed-in and has username */}
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <Image src={user?.photoURL} alt="photo URL image" />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {/* user is not signed OR has not created username */}
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
