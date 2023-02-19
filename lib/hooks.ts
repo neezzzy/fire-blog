f1e91b64593a238b981d7c60a6178c2852d62b6e
Ruslan Nizamov <nizamov.ruslan@gmail.com>
2023-02-18 17:08:28 -0800

    add missing files

---
+++ lib/hooks.ts
@@ -0,0 +1,42 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, doc, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+const auth = getAuth();
+const firestore = getFirestore();
+
+// Custom hook to read auth record and user profile doc
+export function useUserData() {
+  const [user, setUser] = useState(auth.currentUser);
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    // Turn off realtime subscription
+    let unsubscribe: () => void;
+
+    if (user) {
+      const ref = doc(firestore, "users", user.uid);
+      unsubscribe = onSnapshot(ref, (doc) => {
+        setUsername(doc.data()?.username);
+      });
+    } else {
+      setUsername(null);
+    }
+
+    const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+        setUsername(null);
+      }
+    });
+
+    return () => {
+      unsubscribeAuth;
+      unsubscribe;
+    };
+  }, []);
+
+  return { user, username };
+}
