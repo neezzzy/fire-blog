9e5e714fd5c6cc802cf402e3c6f7e7206b81188d
Ruslan Nizamov <nizamov.ruslan@gmail.com>
2023-02-18 21:08:19 -0800

    add images

---
+++ components/UserProfile.tsx
@@ -0,0 +1,21 @@
+// UI component for user profile
+import Image from "next/image";
+import hacker_logo from "../public/hacker.png";
+
+export default function UserProfile({ user }) {
+  return (
+    <div className="box-center">
+      <Image
+        alt="User Profile"
+        width="150"
+        height="150"
+        src={user.photoURL || hacker_logo}
+        className="card-img-center"
+      />
+      <p>
+        <i>@{user.username}</i>
+      </p>
+      <h1>{user.displayName || "Anonymous User"}</h1>
+    </div>
+  );
+}
